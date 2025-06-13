420e6be603f00b9dede4667d3a680d4941d303d8
Zubain-Nadeem <Zubainali23@gmail.com>
2025-06-13 02:42:38 -0700

    Compeleted

--- Level-01-Counter/app.js
+++ Level-01-Counter/app.js
@@ -4,19 +4,17 @@ let sub = document.querySelector("#Sub-btn");
 let changeValue = document.querySelector("#Change-value");
 let resetBtn = document.querySelector("#reset");
 
-add.addEventListener("click" , () => {
-    let countValue = parseInt(countEl.innerText);
-    let changeOn = parseInt(changeValue.value)
-    countEl.innerText = countValue + changeOn
-    
-})
-sub.addEventListener("click" , () => {
-   
-     let countValue = parseInt(countEl.innerText);
-    let changeOn = parseInt(changeValue.value)
-    countEl.innerText = countValue - changeOn
-})
+add.addEventListener("click", () => {
+  let countValue = parseInt(countEl.innerText);
+  let changeOn = parseInt(changeValue.value);
+  countEl.innerText = countValue + changeOn;
+});
+sub.addEventListener("click", () => {
+  let countValue = parseInt(countEl.innerText);
+  let changeOn = parseInt(changeValue.value);
+  countEl.innerText = countValue - changeOn;
+});
 
-resetBtn.addEventListener('click' , () => {
-    countEl.innerHTML = 0
-})
+resetBtn.addEventListener("click", () => {
+  countEl.innerHTML = 0;
+});
