80f5b4efc4710263500af70c4b6cdc161a91ec48
Zubain-Nadeem <Zubainali23@gmail.com>
2025-06-12 23:01:31 -0700

    Ui Compeleted

---
+++ Level-01-Counter/app.js
@@ -0,0 +1,12 @@
+let countEl = document.querySelector("#count");
+let Add = document.querySelector("#Add-btn");
+let Sub = document.querySelector("#Sub-btn");
+
+Add.addEventListener("click" , () => {
+    console.log("Add button");
+    
+})
+Sub.addEventListener("click" , () => {
+    console.log("sub button");
+    
+})
