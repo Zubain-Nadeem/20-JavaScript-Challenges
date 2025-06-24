346f37f316083f7ada68001d0bd0e148bb42389e
Zubain-Nadeem <Zubainali23@gmail.com>
2025-06-23 22:51:15 -0700

    Element added

---
+++ Level-03-String-Transformer/app.js
@@ -0,0 +1,10 @@
+let userInput = document.querySelector("#user-input");
+
+let lowerCaseEl = document.querySelector("#LowerCase")
+let upperCaseEl = document.querySelector("#UpperCase")
+let CamelCaseEl = document.querySelector("#CamelCase")
+let pascalCaseEl = document.querySelector("#PascalCase")
+let snakeCaseEl = document.querySelector("#SnakeCase")
+let kebabCaseEl = document.querySelector("#KebabCase")
+let trimEl = document.querySelector("#Trim")
+
